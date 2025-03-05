f157cc8f5a8c7e843300b8af82eb8537c9623c18
hossein ahmadi <ho3inahmadi138013@gmail.com>
2025-03-05 10:19:58 -0800

    feat : add shadcn uikit to project

--- src/layout/mainLayout.tsx
+++ src/layout/mainLayout.tsx
@@ -1,4 +1,14 @@
+"use client";
 import AppHeader from "@/components/header/appHeader";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
 
 interface Props {
   children: React.ReactNode;
@@ -17,7 +27,8 @@ export function MainLayout({ children, lang, dir }: Props) {
       </div>
       <footer className="bg-white h-20">
         <div className="flex-1 w-full max-w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl mx-auto px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16">
-          <span className="text-red-500">footer</span>
+          <span className="text-primary">footer</span>
+          <Button>Click me</Button>
         </div>
       </footer>
     </div>
